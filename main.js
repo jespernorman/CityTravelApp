497f68f5d5cb0cf8804b8a650b210ab6b73b3a9e
sickshit <sickshit>
2021-02-23 20:04:56 +0100

    js update

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log"TJAAA"
+class search {
+
+    constructor(searchInput) {
+
+    }
+    executeSearch() {
+        fetch('api.openweathermap.org/data/2.5/weather?q={searchInput}&appid={8d141e7ff86b157ab911daed7c1b290b}')
+        .then(Response => {
+            if(Response.ok) {
+                return Response.json()
+            }
+            throw new error('Request failed')
+        }, networkError => {
+            console.log(networkError.message);
+        })
+        .then(jsonResponse => {
+            console.log(jsonRespose)
+        })        
+    }
+}
+
